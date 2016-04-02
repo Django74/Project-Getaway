82df0fd39978e882af219d9a62d355993e0ab60e
dshergill <diljotshergill@hotmail.com>
2016-04-02 15:29:22 -0600

    Private Message Window Created

---
+++ src/getaway/PrivateMessageWindow.java
@@ -0,0 +1,14 @@
+package getaway;
+
+import javax.swing.*;
+
+/**
+ * Created by Diljot on 4/2/2016.
+ */
+public class PrivateMessageWindow {
+    private JTextArea textAreaPrivateMessageDisplay;
+    private JTextField textFieldPrivateMessage;
+    private JButton sendButton;
+    private JList listOnlinePlayers;
+    private JScrollPane scrollPaneListOnlinePlayers;
+}
